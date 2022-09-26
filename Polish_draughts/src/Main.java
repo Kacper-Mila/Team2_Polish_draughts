5605ede679812cc4cc2609f92e6e35b3143c41aa
sandrac91 <sandra.b.ciura@gmail.com>
2022-09-26 13:20:21 +0200

    add removePawn() method

--- Polish_draughts/src/Main.java
+++ Polish_draughts/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 10;
+        Pawn[][] fields;
+
+        Board board = new Board(number);
+        fields = board.getFields();
+        for (int i = 0; i < number;  i++) {
+            System.out.println();
+            for(int j = 0; j < number; j++) {
+                System.out.print("\t " + fields[i][j] + " ");
+            }
+        }
     }
 }
