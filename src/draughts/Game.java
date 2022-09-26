6722cac8c47e0f77186bca18f510ede48056d94c
GasiorMateusz <mateusz.gasior@motorolasolutions.com>
2022-09-26 19:56:50 +0200

    add start(), playRound(), checkForWinner() sketches of methods

--- src/draughts/Game.java
+++ src/draughts/Game.java
@@ -10,5 +10,27 @@ public class Game {
         return this.board;
     }
 
+    /**
+     * method that starts game between players.
+     */
+    public void start(){
+
+    }
+
+    /**
+     *  determines one-round actions that is, checks which player is next and whether
+     *  there is a winner.
+     */
+    public void playRound(){
+
+    }
+
+    /**
+     * method that checks whether there is a winner after each round.
+     * also checks for draws.
+     */
+    public void checkForWinner(){
+
+    }
 
 }
