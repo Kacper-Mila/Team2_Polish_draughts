904be3a2f76c7716206db5a7fd22071b8c1a6e06
Katarzyna Tomczak <katarzyna.ntomczak@gmail.com>
2022-09-26 13:32:05 +0200

    Add removePawn()method

--- Polish_draughts/src/Board.java
+++ Polish_draughts/src/Board.java
@@ -1,3 +1,9 @@
 public class Board {
-
+    public void removePawn(Pawn pawn, Coordinates coordinates){
+        if(this.fielsds[coordinates.getX()][coordinates.getY()].equals(pawn)){
+            fields[coordinates.getX()][coordinates.getY] = null;
+        } else{
+            System.out.println("this Pawn is not in that position");
+        }
+    }
 }
