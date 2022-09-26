75497276397f04075cf2f8619900bc1e82c490db
GasiorMateusz <mateusz.gasior@motorolasolutions.com>
2022-09-26 16:33:38 +0200

    fix merge chaos
    changed Game.java and pawn.java files to be consistent with Board.Java

--- Polish_draughts/src/draughts/Game.java
+++ Polish_draughts/src/draughts/Game.java
@@ -2,4 +2,13 @@ package draughts;
 
 public class Game {
 
+    private Board board;
+    public Game(int n){
+        this.board = new Board(n);
+    }
+    public Board getBoard(){
+        return this.board;
+    }
+
+
 }
