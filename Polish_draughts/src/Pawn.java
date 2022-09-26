4e81cc10b0a1f307df7b99b31d98a6e3614d5c5e
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2022-09-26 23:29:53 +0200

    Add getColor method to Pawn class

---
+++ Polish_draughts/src/Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn {
+
+    public String getColor(int player){
+        if (player == 1) {
+            return "white";
+        } else {
+            return "black";
+        }
+    }
+
+}
