8dbfdf0138c65ad6c8c2f645882f47e3a39d0059
GasiorMateusz <mateusz.gasior@motorolasolutions.com>
2022-09-28 13:24:35 +0200

    merge kasia into main

--- src/draughts/Board.java
+++ src/draughts/Board.java
@@ -1,7 +1,8 @@
 package draughts;
 
-import static java.awt.Color.black;
-import static java.awt.Color.white;
+import java.awt.*;
+
+import static java.awt.Color.*;
 
 public class Board {
     private Pawn[][] fields;
@@ -54,54 +55,72 @@ public class Board {
     /**
      * Print current board.
      * This method marks rows as numbers and columns as letters.
-     *
      * @return String representing board
      */
     @Override
     public String toString() {
         final String ANSI_GREY_BACKGROUND = "\u001B[47m";
         final String ANSI_WHITE_BACKGROUND = "\u001B[107m";
+        final String ANSII_BLACK_BACKGROUND = "\u001B[40m";
         final String ANSI_BLACK = "\u001B[30m";
-        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_RESET = "\u001B[0m"; //resets all formatting
         final String ANSI_WHITE = "\u001B[97m";
+        final String ANSI_GREY = "\u001B[37m";
         final String ANSI_BOLD = "\u001B[1m";
-        Pawn[][] board = this.fields;
+        final String ANSI_PAWN= "\u2659";
+        final String ANSI_QUEEN= "\u265B";
+        final String FORMAT = "%-3.40s"; //String format that keeps field width min 3 chars and max 40.
+        // Its 40 because invisible formatting escape symbols are included in string length
+        Pawn [][] board = this.fields;
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < board.length + 1; i++) {
-            for (int j = 0; j < board[0].length + 1; j++) {
-                if (i > 0 && j > 0) {
-                    String pawn = " ";
-                    if (board[i - 1][j - 1] != null) {
-                        if (board[i - 1][j - 1].isCrowned()) {
-                            pawn = "X";
-                        } else {
-                            pawn = "O";
+        Color background;
+        for(int i =0;i<board.length+1;i++){
+            for(int j=0;j<board[0].length+1;j++){
+                if(i>0&&j>0){
+                    String pawn = "";
+                    if(board[i-1][j-1]!=null){
+                        if(board[i-1][j-1].isCrowned()){
+                            pawn = ANSI_QUEEN; //Crowned pawn symbol
+                        }else{
+                            pawn = ANSI_PAWN;//pawn symbol
                         }
-                        if (board[i - 1][j - 1].getColor() == black) {
-                            pawn = ANSI_BOLD + ANSI_WHITE + " " + pawn + " ";
-                        } else {
-                            pawn = ANSI_BOLD + ANSI_BLACK + " " + pawn + " ";
-                        }
-                    }
-                    if (i % 2 == 0) {
-                        if (j % 2 == 0) {
-                            result.append(ANSI_WHITE_BACKGROUND);
-                        } else {
-                            result.append(ANSI_GREY_BACKGROUND);
-                        }
-                    } else {
-                        if (j % 2 == 0) {
-                            result.append(ANSI_GREY_BACKGROUND);
-                        } else {
-                            result.append(ANSI_WHITE_BACKGROUND);
+                        if(board[i-1][j-1].getColor()==black) { //set pawns color on the board according to color set
+                            pawn =  ANSI_BLACK  +" "+ pawn + " "; //set pawn's color black
+                        }else{
+                            pawn =  ANSI_WHITE +" "+ pawn+" "; //set pawn's color white
                         }
                     }
-                    result.append(String.format("%3.40s", pawn)).append(ANSI_RESET);
-                } else {
+                    if(i%2==0) { //even row
+                        if (j % 2 == 0) {
+                            result.append(ANSI_WHITE_BACKGROUND);
+                            background = WHITE;
+                        } else {
+                            result.append(ANSI_GREY_BACKGROUND);
+                            background =BLACK;
+                        }
+                    }else{ //odd row
+                        if (j % 2 == 0) {
+                            result.append(ANSI_GREY_BACKGROUND);
+                            background = BLACK;
+                        } else {
+                            result.append(ANSI_WHITE_BACKGROUND);
+                            background = WHITE;
+                        }
+                    }
+                    if(pawn.equals("")){ //if field has no pawn then add circle with color of the background to keep
+                        // fields alignment
+                        if(background==black){
+                            pawn = " "+ANSI_GREY+ ANSI_PAWN + " "; //empty field
+                        }else{
+                            pawn = " "+ANSI_WHITE + ANSI_PAWN + " "; //empty field
+                        }
+                    }
+                    result.append(String.format(FORMAT,ANSI_BOLD+pawn)).append(ANSI_RESET);
+                }else{
                     char col = (char) (64 + j);
-                    if (i == 0 && j == 0) result.append("   ");
-                    if (i == 0 && j > 0) result.append(String.format("%-3.3s", " " + col));
-                    if (j == 0 && i > 0) result.append(String.format("%-3.3s", i));
+                    if(i==0&&j==0) result.append(String.format(FORMAT,"   "));//empty field in left up corner
+                    if(i==0&&j>0) result.append(String.format(FORMAT,ANSII_BLACK_BACKGROUND+ANSI_BOLD+ " "+col+ANSI_BLACK+ANSI_PAWN+ANSI_RESET));// Letters row
+                    if(j==0&&i>0) result.append(String.format(FORMAT,i));// Numbers column
                 }
             }
             result.append("\n");
@@ -118,6 +137,7 @@ public class Board {
     }
 
     public void removePawn(Pawn pawn) {
+        //zmniejsza o 1 licznik pionkow w klasie board, zgodnie z kolorem jaki zawiera obiekt pawn
         int x = pawn.getPosition().getX();
         int y = pawn.getPosition().getY();
         this.fields[x][y] = null;
@@ -127,7 +147,7 @@ public class Board {
     }
 
 
-    public void movePawn(Pawn pawn, Coordinates position) {
+    public void movePawn(Pawn pawn, Coordinates position){
         //There is a movePawn() method that moves pawns from a specified position to another field.
         //This method is just changing pawns coordinates
         //removes pawn from startPosition and moves it to endPosition
