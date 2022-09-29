f11ddc02f95b3e6ea964c08b74307ff7a6112827
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2022-09-29 10:19:40 +0200

    Added getBoardSize method

--- src/draughts/Main.java
+++ src/draughts/Main.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(10);
+        Game game = new Game(getBoardSize());
         game.start();
+    }
+    private static int getBoardSize() {
+        Scanner readBoardSize= new Scanner(System.in);
+        String boardSize;
+        String regex = "1[0-9]|20";
+        do {
+            System.out.println("Podaj wielkosc tablicy z zakresu od 10 do 20");
+            boardSize = readBoardSize.nextLine();
+        }while(!boardSize.matches(regex));
+        return Integer.parseInt(boardSize);
     }
 }
