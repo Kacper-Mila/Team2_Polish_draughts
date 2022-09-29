41bad2d338503810d476af6eaacdde3e97917855
sandrac91 <sandra.b.ciura@gmail.com>
2022-09-29 09:33:29 +0200

    Edit validateMove()

--- src/draughts/Main.java
+++ src/draughts/Main.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Polish draughts!");
-        System.out.println("Please enter length of the board side (between 10 and 20");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
         Game game = new Game(10);
         game.start();
     }
